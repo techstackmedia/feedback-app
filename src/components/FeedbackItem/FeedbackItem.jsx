cf8764c116e23084e56ab8f9e9d9aa195cf93f58
techstackmedia <osagienoah04@gmail.com>
2022-05-08 14:12:56 +0100

    children props in card ui

--- src/components/FeedbackItem/FeedbackItem.jsx
+++ src/components/FeedbackItem/FeedbackItem.jsx
@@ -1,6 +1,8 @@
 import Card from '../../UI/Card';
 const FeedbackItem = ({ feedbackItem }) => {
   return (
+    // without "className='random'", we get <div class="card undefinded"></div>
+    // undefined as a class (.undefined)
     <Card>
       <div className="num-display">{feedbackItem.rating}</div>
       <div className="text-display">{feedbackItem.text}</div>
