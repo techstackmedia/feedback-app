9c333f323e0ce2712c02743a464b69680a2dc659
techstackmedia <osagienoah04@gmail.com>
2022-05-08 15:06:43 +0100

    create a conditional style

--- src/UI/Card.jsx
+++ src/UI/Card.jsx
@@ -1,5 +1,15 @@
-const Card = ({ children, reverse }) => {
-  return <div className={`card ${reverse && 'reverse'}`}>{children}</div>;
+const Card = ({ children, className, reverse }) => {
+  const classReverser = {
+    backgroundColor: reverse ? 'rgba(0, 0, 0, 0.4)' : '#fff',
+    color: reverse ? '#fff' : '#000',
+  };
+  const classes = `card ${className}`;
+
+  return (
+    <div className={classes} style={classReverser}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
