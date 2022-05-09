ee08640db683b1d62d933b443990fca2b9bbd31b
techstackmedia <osagienoah04@gmail.com>
2022-05-09 01:02:32 +0100

    toggle mode light/dark

--- src/components/FeedbackItem/FeedbackItem.jsx
+++ src/components/FeedbackItem/FeedbackItem.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import { FaRegLightbulb } from "react-icons/fa";
+import { FaLightbulb } from "react-icons/fa";
 import PropTypes from 'prop-types';
 import { FaTimes } from 'react-icons/fa';
 import Card from '../../UI/Card';
 const FeedbackItem = ({ feedbackItem, handleClick }) => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  const handleModeClick = () => {
+    setIsDarkMode((prev) => {
+      return !prev;
+    });
+  };
+
   return (
-    <Card>
+    <Card reverse={isDarkMode}>
+      <button onClick={handleModeClick}>
+        {isDarkMode ? <FaRegLightbulb /> : <FaLightbulb />}
+      </button>
       <div className="num-display">{feedbackItem.rating}</div>
       <button className="close" onClick={() => handleClick(feedbackItem.id)}>
         <FaTimes color="purple" />
