2c133bce0e296bfb32b16a33cf0015a2bd661810
techstackmedia <osagienoah04@gmail.com>
2022-06-26 14:35:44 +0100

    implement a form real time validation

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -3,7 +3,20 @@ import Card from '../../Customs/Card';
 import Button from '../../Customs/Button';
 const FeedbackForm = () => {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
+
   const handleTextChange = e => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length < 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be more than 10 characters');
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
     setText(e.target.value);
   };
   return (
@@ -18,8 +31,12 @@ const FeedbackForm = () => {
             value={text}
             onChange={handleTextChange}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
