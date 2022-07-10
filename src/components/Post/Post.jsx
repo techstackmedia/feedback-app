39f30c4977ef70305f692620b103f86543ba03ae
techstackmedia <osagienoah04@gmail.com>
2022-07-10 18:42:48 +0100

    commit changes in react-router-dom-useNavigate

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,11 +1,21 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const Post = () => {
   const status = 202;
+  const navigate = useNavigate();
+  const handleRedirectClick = () => {
+    navigate('/about');
+  };
+  
   if (status === 404) {
     return <Navigate to="/notfound" />;
   }
-  return <h1>Post</h1>;
+  return (
+    <>
+      <h1>Post</h1>
+      <button onClick={handleRedirectClick}>Click</button>
+    </>
+  );
 };
 
 export default Post;
