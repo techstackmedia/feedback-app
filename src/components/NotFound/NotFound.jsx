c2ebc8694675588a572b14f4dad6afa49ff5c03b
techstackmedia <osagienoah04@gmail.com>
2022-07-10 18:23:22 +0100

    react-router-dom Navigate

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   const centerText = {
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     height: '100vh',
+  };
+
+  const linkColor = {
+    color: '#00f',
+    textDecoration: 'none',
+    backgroundColor: '#fff',
+    padding: 5,
+    borderRadius: 4
   };
   return (
     <div style={centerText}>
       <h1>
         This page is <b>not found</b>
       </h1>
+      <Link to="/" style={linkColor}>
+        Back to Home
+      </Link>
     </div>
   );
 };
