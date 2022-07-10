312438678f87810be0cf0320495b1c89d8d61e24
techstackmedia <osagienoah04@gmail.com>
2022-07-10 22:58:42 +0100

    add feedback data to server and set up proxy

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 const FeedbackProvider = ({ children }) => {
@@ -15,16 +14,22 @@ const FeedbackProvider = ({ children }) => {
   }, []);
 
   const fetchFeedback = async () => {
-    const response = await fetch(
-      'http://localhost:5000/feedback?_sort=id&_order=asc'
-    );
+    const response = await fetch('/feedback?_sort=id&_order=desc');
     const data = await response.json();
     setFeedback(data);
     setIsLoading(false);
   };
-  const clickAddHandler = newFeedback => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const clickAddHandler = async newFeedback => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    });
+    const data = await response.json();
+    // newFeedback.id = uuidv4();
+    setFeedback([data, ...feedback]);
   };
   const clickDeleteHandler = id => {
     if (window.confirm('Are you sure you want to delete?')) {
