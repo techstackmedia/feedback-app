0775e37e778da9155e114c19df3e0289f67ccf15
techstackmedia <osagienoah04@gmail.com>
2022-07-12 14:07:49 +0100

    commit changes

--- src/components/FeedbackItem/FeedbackItem.jsx
+++ src/components/FeedbackItem/FeedbackItem.jsx
@@ -1,8 +1,6 @@
 import Card from '../../UI/Card';
 const FeedbackItem = ({ feedbackItem }) => {
   return (
-    // without "className='random'", we get <div class="card undefinded"></div>
-    // undefined as a class (.undefined)
     <Card>
       <div className="num-display">{feedbackItem.rating}</div>
       <div className="text-display">{feedbackItem.text}</div>
