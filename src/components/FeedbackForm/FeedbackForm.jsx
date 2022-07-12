f9eb0da3d7c3dbf12bc47eb3887a61824e29d44a
techstackmedia <osagienoah04@gmail.com>
2022-07-12 22:15:17 +0100

    add create context and use context hook

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import Card from '../../Customs/Card';
-import Button from '../../Customs/Button';
+import Card from '../../shared/Card';
+import Button from '../../shared/Button';
 import RatingSelect from '../RatingSelect/RatingSelect';
 
 const FeedbackForm = ({ handleAdd }) => {
