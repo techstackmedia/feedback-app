42f184bbad54bec36a68c63fe7e94d3559ede9b4
techstackmedia <osagienoah04@gmail.com>
2022-07-12 17:46:19 +0100

    revisit text input form field

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react';
 import Card from '../../Customs/Card';
 import Button from '../../Customs/Button';
+
 const FeedbackForm = () => {
   const [text, setText] = useState('');
+  const [message, setMessage] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
   const handleTextChange = e => {
+    if (text.trim().length < 0) {
+      setMessage(null);
+      setBtnDisabled(true);
+    } else if (text.trim().length < 10) {
+      setMessage('Text must be more than 10 characters');
+      setBtnDisabled(true);
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+
     setText(e.target.value);
   };
+
   return (
     <Card>
       <h2>How would rate your service with us?</h2>
@@ -18,8 +33,11 @@ const FeedbackForm = () => {
             value={text}
             onChange={handleTextChange}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        <div className="message">{message}</div>
       </form>
     </Card>
   );
