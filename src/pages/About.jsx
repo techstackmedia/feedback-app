20610b68b647b84c3be17823e54c628fb17551b0
techstackmedia <osagienoah04@gmail.com>
2022-07-13 00:37:40 +0100

    commit changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,21 +1,31 @@
 import { Link } from 'react-router-dom';
 import Card from '../shared/Card';
 import Header from '../components/Header/Header';
+import { AnimatePresence, motion } from 'framer-motion';
+
 const About = () => {
   return (
     <>
       <Header />
       <div className="container">
-        <Card>
-          <div className="about">
-            <h1>About This Project</h1>
-            <p>
-              This is a React app to leave feedback for a product or service
-            </p>
-            <p>Version 1.0.0</p>
-            <Link to="/">Back to Home</Link>
-          </div>
-        </Card>
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ ease: 'easeInOut', duration: 0.3 }}
+          >
+            <Card>
+              <div className="about">
+                <h1>About This Project</h1>
+                <p>
+                  This is a React app to leave feedback for a product or service
+                </p>
+                <p>Version 1.0.0</p>
+                <Link to="/">Back to Home</Link>
+              </div>
+            </Card>
+          </motion.div>
+        </AnimatePresence>
       </div>
     </>
   );
