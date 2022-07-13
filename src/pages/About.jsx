b2092e7491027402d191cceb028e10ca3b35e576
techstackmedia <osagienoah04@gmail.com>
2022-07-14 00:17:30 +0100

    commit changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
-            transition={{ ease: 'easeInOut', duration: 0.3 }}
+            exit={{ opacity: 0, scale: 0 }}
           >
             <Card>
               <div className="about">
